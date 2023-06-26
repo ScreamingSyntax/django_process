8503f060d698ea25aef50656b930ad2caa003a1a
Aaryan Jha <whcloud91@gmail.com>
2023-06-26 23:04:02 +0545

    Usin Django Templates

--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('Say Hello')
+    return render(request,'index.html',{'name':'Aaryan jha'})
 # Create your views here.
