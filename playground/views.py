3ae2a653847c71a1901338fbb4b82cab184fc846
Aaryan Jha <whcloud91@gmail.com>
2023-06-26 23:24:48 +0545

    Added Templates and Learned Break Points

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def calculate():
+    x=1
+    y=2
+    return x
+
 def say_hello(request):
+    x = calculate()
     return render(request,'index.html',{'name':'Aaryan jha'})
 # Create your views here.
